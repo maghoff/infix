a1a8b30368fbb4cc890d2bae4d6253dda86940c0
Magnus Hoff <maghoff@gmail.com>
2016-03-05 00:49:08 +0100

    Implement support for writing out a parsed expression in the source language

--- ast.js
+++ ast.js
@@ -1,3 +1,10 @@
+var precedence = {
+	'+': 1,
+	'-': 1,
+	'*': 2,
+	'/': 2,
+};
+
 function BinaryOperation(lhs, op, rhs) {
 	this.lhs = lhs;
 	this.op = op;
@@ -9,12 +16,22 @@ BinaryOperation.prototype.asJS = function (constName, paramName) {
 		"(" + this.lhs.asJS(constName, paramName) + ", " + this.rhs.asJS(constName, paramName) + ")";
 };
 
+BinaryOperation.prototype.asExpression = function (parentPrecedence) {
+	var parenthesis = precedence[this.op] < parentPrecedence;
+	var core = this.lhs.asExpression(precedence[this.op]) + this.op + this.rhs.asExpression(precedence[this.op] + 1);
+	return parenthesis ? "(" + core + ")" : core;
+};
+
 function IntegerLiteral(literal) {
 	this.literal = literal;
 }
 
 IntegerLiteral.prototype.asJS = function (constName) {
 	return constName(this.literal, "np.parseInt('" + this.literal + "')");
+};
+
+IntegerLiteral.prototype.asExpression = function () {
+	return this.literal;
 };
 
 function DecimalLiteral(literal, wholePart, decimalPart) {
@@ -27,12 +44,20 @@ DecimalLiteral.prototype.asJS = function (constName) {
 	return constName(this.literal, "np.parseDecimal('" + this.wholePart + "', '" + this.decimalPart + "')");
 };
 
+DecimalLiteral.prototype.asExpression = function () {
+	return this.literal;
+};
+
 function Reference(name) {
 	this.name = name;
 }
 
 Reference.prototype.asJS = function (constName, paramName) {
 	return paramName(this.name);
+};
+
+Reference.prototype.asExpression = function () {
+	return "$" + this.name;
 };
 
 var parseHandler = {
