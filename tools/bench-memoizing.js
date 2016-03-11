7c0f2498bb6a7f8ec7beaaad7e0aedfd68e48397
Magnus Hoff <maghoff@gmail.com>
2016-03-11 11:28:25 +0100

    Improve benchmarking script

--- tools/bench-memoizing.js
+++ tools/bench-memoizing.js
@@ -2,49 +2,57 @@
 
 var infix = require('..');
 
-function explicitlyCompile() {
+function explicitlyCompile(iterations) {
 	var f = infix.compile("100 - $0", infix.nativeNumberProvider);
 
 	var x = 0;
-	for (var i = 0; i < 100000; i++) {
+	for (var i = 0; i < iterations; i++) {
 		x += f(i);
 	}
 	return x;
 }
 
-function memoizing() {
+function memoizing(iterations) {
 	var evaluate = infix.memoizing.evaluatorFor(infix.nativeNumberProvider);
 
 	var x = 0;
-	for (var i = 0; i < 100000; i++) {
+	for (var i = 0; i < iterations; i++) {
 		x += evaluate("100 - $0", i);
 	}
 	return x;
 }
 
-function nonMemoizing() {
+function nonMemoizing(iterations) {
 	var evaluate = infix.evaluatorFor(infix.nativeNumberProvider);
 
 	var x = 0;
-	for (var i = 0; i < 100000; i++) {
+	for (var i = 0; i < iterations; i++) {
 		x += evaluate("100 - $0", i);
 	}
 	return x;
 }
 
-function time(inner) {
+function time(inner, iterations) {
 	var start = Date.now();
-	inner();
+	inner(iterations);
 	return Date.now() - start;
 }
 
-function trial(inner) {
-	console.log("Measuring " + inner.name);
+function trial(inner, iterations) {
+	process.stdout.write("Measuring " + inner.name);
+	var times = [];
 	for (var i = 0; i < 10; ++i) {
-		console.log(time(inner));
+		times.push(time(inner, iterations));
 	}
+	times.sort();
+	var avg = times.slice(2, -2).reduce(function (a, b) { return a+b; }) / 6 / iterations;
+	console.log(" => " + (avg * 1000000).toFixed(2) + "ns per iteration");
+	return avg;
 }
 
-trial(explicitlyCompile);
-trial(memoizing);
-trial(nonMemoizing);
+var a = trial(explicitlyCompile, 10000000);
+var b = trial(memoizing, 100000);
+var c = trial(nonMemoizing, 50000);
+
+console.log(`explicitlyCompile is ${(b/a).toFixed(2)} times as fast as memoizing`);
+console.log(`memoizing is ${(c/b).toFixed(2)} times as fast as nonMemoizing`);
